ad4abe8c533473e324f46793f8af1dd99302fe19
dany wilkings de leon duverge <danydeleon26@gmail.com>
2017-10-16 16:08:15 -0500

    mi primer maldito commit

---
+++ PrestaMe/V1.0/Sources/User Interfaces/Windows/PrestaMe.Windows/Form1.cs
@@ -0,0 +1,30 @@
+﻿#region     CopyRight
+#endregion  CopyRight
+#region     Uso e Invocación de Librerías de Clase a Ser Utilizadas
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using PrestaMe.Layers.Application;
+#endregion  Uso e Invocación de Librerías de Clase a Ser Utilizadas
+
+
+#region     Logica de la Clase, Según NameSpace Especificado
+
+namespace OdontoMatic.Forms
+{
+    #region    Estructura de Metodos, Propiedades, Procedimientos y Funciones de la Clase Indicada
+    /// <summary>    
+    /// /// Estructura de Metodos, Propiedades, Procedimientos y Funciones de la Clase Indicada    
+    /// /// </summary>
+    public partial class FormSplash : Form
+    {
+        #region    Constructor Principal Sin Parámetros Invocados
+        /// <summary>       
+        /// Constructor Sin Parámetros      
+        /// 
+        /// </summary>       
+
+        public FormSplash() 
+
